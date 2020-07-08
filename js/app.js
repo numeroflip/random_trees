fb3caf696dad1e6a3cba42748b3b6c225962b7dd
numeroflip <berenyi.aron@hotmail.hu>
2020-07-08 21:03:38 +0200

    starting the content, layout, design and dark/light themes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-document.querySelector('body').style.backgroundColor = 'red';
+const html = document.querySelector('html');
+
+/* Dark/Light theme controller */
+const lightBtn = document.querySelector('#light-button');
+const darkBtn = document.querySelector('#dark-button');
+const themeBtns = document.querySelector('.theming-btns');
+
+/* Add 4 random tree sections to the empty main page */
+function addPost (num) {
+    const postHtml = ``
+    document.querySelector('.sections').insertAdjacentHTML()
+}
+
+function init {
+
+}
+
+themeBtns.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target == lightBtn && html.dataset.theme == 'dark') {
+        html.dataset.theme = 'light';
+    } 
+    else if ( target == darkBtn && html.dataset.theme == 'light' ) {
+        html.dataset.theme = 'dark';
+    }
+})
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Landing Page
+## What is it?
+## Technologies used
+* HTML
+* CSS, SASS
+* JavaScript
+## But... Why?
+I started learning web development a while ago, followed a bunch of tutorials, but never felt confident enough with my skills. Now I enrolled for the free month of Udacity's [Front End nanodegree](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011), to check it out. 
+## Random quote
+
+> “I wish I could write as mysterious as a cat.”  
+> <cite> -E. A. Poe </cite>
+## Conclusion
